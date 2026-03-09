d93cb3d6f5f8e6a597ce1bc14cf1eae2df72fc52
Martin Olsson <martinolsson89@gmail.com>
2026-03-09 09:57:00 +0100

    Add project files.

---
+++ Industrial_Fleet_Manager/Industrial_Fleet_Manager.Client/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Industrial_Fleet_Manager.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
