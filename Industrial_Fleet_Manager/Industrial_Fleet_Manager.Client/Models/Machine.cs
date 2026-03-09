8b1f57aa0f929f6b35e5b2a3d265cbe2a98a9802
Martin Olsson <martinolsson89@gmail.com>
2026-03-09 11:14:22 +0100

    Modernize layout: add NavBar, remove sidebar NavMenu
    Refactored MainLayout to use a sticky top NavBar with Bootstrap styling, replacing the sidebar NavMenu. Removed NavMenu component and its CSS. Introduced NavBar component with customizable title and right-aligned actions. Added Machine model class and updated _Imports.razor for Models namespace. Created Index.razor dashboard page. Prepares app for machine-related features and improves navigation UX.

---
+++ Industrial_Fleet_Manager/Industrial_Fleet_Manager.Client/Models/Machine.cs
@@ -0,0 +1,10 @@
+﻿namespace Industrial_Fleet_Manager.Client.Models;
+
+public class Machine
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public bool IsOnline { get; set; }
+    public string LastData { get; set; }
+    public DateTime LastUpdated { get; set; }
+}
