282525fb0ffae75520fa43282efebefaade7b03e
Martin Olsson <martinolsson89@gmail.com>
2026-03-11 11:23:22 +0100

    Add daily stats chart for online machines
    Integrated Blazor-ApexCharts to visualize daily machine stats.
    MachineStatusSnapshot model tracks online count history.
    StatsComponent now displays area chart of online machines.
    Snapshots are recorded on add, toggle, edit, and delete actions.

---
+++ Industrial_Fleet_Manager/Industrial_Fleet_Manager.Client/Models/MachineStatusSnapshot.cs
@@ -0,0 +1,8 @@
+﻿namespace Industrial_Fleet_Manager.Client.Models;
+
+public class MachineStatusSnapshot
+{
+    public int Sequence { get; set; }
+    public DateTime Timestamp { get; set; }
+    public int OnlineCount { get; set; }
+}
